60763ff01c14f979a70bc533337323448fc717aa
Thesonggodoftoliet <535644752@qq.com>
2019-01-31 20:36:36 +0800

    同上次提交

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ App({
                   wx.redirectTo({
                     url: '../register/register'
                   })
-                else if (res.user_info.role == 0) {
+                else if (res.user_info.role == 0) {//成员身份为用户
                   wx.redirectTo({
                     url: '/pages/user/user',
                   })
                 }
-                else if (res.user_info.role == 1) {
+                else if (res.user_info.role == 1) {//成员身份为队员
                   wx.redirectTo({
                     url: '/pages/member/member',
                   })
@@ -33,11 +33,11 @@ App({
               }, 2000)
             }
           }, function (res) {
-            // console.log('端口调用失败' + res.err_msg)
+             console.log('端口调用失败' + res.err_msg)
           })
         }
         else {
-          // console.log('端口调用失败' + res.err_msg)
+           console.log('端口调用失败' + res.err_msg)
         }
       }
     })
@@ -51,7 +51,7 @@ App({
   },
   wxRequest(method, url, data, callback, errFun = null) {
     // console.log(data)
-    let api_key = wx.getStorageSync('api_key')
+    let api_key = wx.getStorageSync('api_key');//同步取出api_key
     // console.log(api_key)
     wx.request({
       url: getApp().globalData.baseURL + url + '?api_key=' + api_key,
@@ -72,80 +72,4 @@ App({
       }
     })
   }
-=======
-//app.js
-App({
-  onLaunch: function () {
-    var that = this
-
-    wx.login({
-      success(res) {
-        if (res.code) {
-          //发起网络请求
-          getApp().wxRequest('POST', '/auth/login/', { code: res.code }, function (res) {
-            if (res.result_code == 1 || res.result_code == 2) {
-              getApp().globalData.user_info = res['user_info'];
-              console.log(res);
-              wx.setStorage({ //本地缓存
-                key: 'api_key',
-                data: res['api_key']
-              });
-              setTimeout(function () {
-                if (res['result_code'] == 2) //判断是否完善信息
-                  wx.redirectTo({
-                    url: '../register/register'
-                  })
-                else if (res.user_info.role == 0) {
-                  wx.redirectTo({
-                    url: '/pages/user/user',
-                  })
-                }
-                else if (res.user_info.role == 1) {
-                  wx.redirectTo({
-                    url: '/pages/member/member',
-                  })
-                }
-              }, 2000)
-            }
-          }, function (res) {
-            // console.log('端口调用失败' + res.err_msg)
-          })
-        }
-        else {
-          // console.log('端口调用失败' + res.err_msg)
-        }
-      }
-    })
-  },
-
-  globalData: {
-    baseURL: 'https://bt.yuancl.site',
-    // baseURL: 'http://192.168.1.115:5000',
-    userInfo: 'hello',
-    user_info: null,
-  },
-  wxRequest(method, url, data, callback, errFun = null) {
-    // console.log(data)
-    let api_key = wx.getStorageSync('api_key')
-    // console.log(api_key)
-    wx.request({
-      url: getApp().globalData.baseURL + url + '?api_key=' + api_key,
-      method: method,
-      data: data,
-      header: {
-        'content-type': 'application/json',
-        'Accept': 'application/json'
-      },
-      dataType: 'json',
-      success: function (res) {
-        callback(res.data);
-      },
-      fail: function (err) {
-        if (errFun) {
-          errFun(err);
-        }
-      }
-    })
-  }
->>>>>>> Stashed changes
 })
